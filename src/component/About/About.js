4accc2d63b7bf952e3816d4ed7b7183a62b3e2c9
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-10-18 10:21:23 -0400

    fix

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -5,7 +5,7 @@ function About () {
         <div className="about">
         <div className="about-container">
             <div className="about-text">
-            <h1>A little more information...</h1>
+            <h2>A little more information...</h2>
             <p>
                 Trevor is a full stack developer with a creative eye and passion for the craft.
                 Trevor has a background in broadcasting and also provides photography and drone 
