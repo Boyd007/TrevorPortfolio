f073c771491c7b0b253a4c1ca6430afd2e651a68
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-10-19 02:04:28 -0400

    projfixes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import Contact from "./component/Contact Form/Contactform";
 import About from "./component/About/About";
 import Projects from "./component/Projects/Projects";
 
-// import Page from "./component/Page/Page";
 import { useState } from "react";
 import Footer from "./component/Footer/Footer";
 import Nav from "./component/Nav/Nav";
-// import Footerwaves from "./component/FooterWaves/Footerwaves";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -17,15 +15,11 @@ function App() {
       <Nav />
 
       <Routes>
-        {/* {renderpage()} */}
         <Route path="/" element={<About />} />
-    
+
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/resume" element={<Resume />} /> */}
-        {/* <Route path="/resources" element={<Resources />} /> */}
       </Routes>
-      {/* <Footerwaves /> */}
       <Footer />
     </>
   );
